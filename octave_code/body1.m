704c5fd96a1e2b8a2b69e0c64368ebbbd8d11066
SimplyOnMyWay <micheal.ocathain@gmail.com>
2022-08-08 18:27:41 +0100

    first stab at filtering warped FR

--- octave_code/body1.m
+++ octave_code/body1.m
@@ -28,10 +28,13 @@ pS = unwrap(angle(S));
 pSi = interp1(fkkB,pS,fkk)';
 Si = Simag.*(cos(pSi)+i*sin(pSi));
 Sfulli = [Si' flip(Si(2:end-1)')]';
-## Smag = 10.^(SdB./20);
-## Sr = Smag.*(cos(pS)+i*sin(pS));
-## Sfullr = [Sr' flip(Sr(2:end-1)')]';
 
+## recover original spectrum from SdB, for referance
+Smag = 10.^(SdB./20);
+Sr = Smag.*(cos(pS)+i*sin(pS));
+Sfullr = [Sr' flip(Sr(2:end-1)')]';
+
+## plot the original against the Bark FR
 figure;
 set(gcf, 'Position', get(0, 'Screensize'));
 plot(fk,[Sfull,Sfulli]);
@@ -44,12 +47,16 @@ signalr = ifft(Sfull);
 # time axes...
 T = 1/fs;
 t = T:T:T*length(signal);
+
+## plot both the recovered and the bark warped against the original
 figure;
 set(gcf, 'Position', get(0, 'Screensize'));
 plot(t,[signal,signalr(1:length(signalr)/8),signali(1:length(signalr)/8)]);
 grid minor on;
 legend("original","recovered","Bark warped!");
 
+
+## plot both linear and Bark-warped FR for comparison
 xlimval = 400;
 df = fkk(2) - fkk(1);
 ixlim = find(abs(fkk - xlimval) < df/2);
@@ -65,6 +72,75 @@ plot(fkkB,SdBNorm);
 ## xlim([0,fkkB(ixlim)]);
 ylim([-60,0]);
 grid minor on;
+
+## zoom in on LF portion of Bark-warped FR, this time using fkk as x-axis
+figure;
+set(gcf, 'Position', get(0, 'Screensize'));
+plot(fkk./1000,SdBNormi);
+axis([0 1.4 -60 0]);
+grid on;
+
+## inverse filtering mode 1
+
+freq = 469;  % estimated peak frequency in Hz
+bw = 10;        % peak bandwidth estimate in Hz
+
+R = exp( - pi * bw / fs);            % pole radius
+z = R * exp(j * 2 * pi * freq / fs); % pole itself
+B = [1, -(z + conj(z)), z * conj(z)] % inverse filter numerator
+r = 0.95;     % zero/pole factor (notch isolation)
+A = B .* (r .^ [0 : length(B)-1]);   % inverse filter denominator
+
+residual = filter(B,A,signali);       % apply inverse filter
+
+Sfull1 = fft(residual,Nfft);
+S1 = Sfull1(iposFreq);
+S1dB = 20*log10(abs(S1));
+## offset = max(S1dB);
+S1dBNorm = S1dB - offset;
+
+figure;
+set(gcf, 'Position', get(0, 'Screensize'));
+plot(fkk./1000,[SdBNormi,S1dBNorm]);
+axis([0 1.4 -60 0]);
+grid on;
+
+
+## inverse filtering mode 2
+
+freq = 915;  % estimated peak frequency in Hz
+bw = 40;        % peak bandwidth estimate in Hz
+
+R = exp( - pi * bw / fs);            % pole radius
+z = R * exp(j * 2 * pi * freq / fs); % pole itself
+B2 = [1, -(z + conj(z)), z * conj(z)] % inverse filter numerator
+r = 0.9;     % zero/pole factor (notch isolation)
+A2 = B2 .* (r .^ [0 : length(B2)-1]);   % inverse filter denominator
+
+residual2 = filter(B2,A2,residual);       % apply inverse filter
+
+Sfull2 = fft(residual2,Nfft);
+S2 = Sfull2(iposFreq);
+S2dB = 20*log10(abs(S2));
+## offset = max(S1dB);
+S2dBNorm = S2dB - offset;
+
+figure;
+set(gcf, 'Position', get(0, 'Screensize'));
+plot(fkk./1000,[SdBNormi,S1dBNorm,S2dBNorm]);
+axis([0 1.4 -60 0]);
+grid on;
+
+
+## plot filtered signals in time domain
+figure;
+set(gcf, 'Position', get(0, 'Screensize'));
+itime = 1:length(residual)/8;
+plot(t,[signali(itime), residual(itime), residual2(itime)]);
+legend("warped original","mode1 removed","mode2 removed");
+
+
+
 
 
 
