965ebb736946e300d019d47ef1242c97d0aced13
SimplyOnMyWay <micheal.ocathain@gmail.com>
2023-02-21 14:57:15 +0000

    ARE etc working! I.e., ARE benchmarked against Faust :)

--- octave_code/run_asrfe.m
+++ octave_code/run_asrfe.m
@@ -5,11 +5,19 @@
 clear all;
 ## close all;                      
 
+load "faustout.mat" faustout;
+
+
 printf("running...!\n");
 ## asrfe envelope
 
 global SR = 48000;
 global fRec0 = [0,0];
+
+
+global zeroCounter = 0;
+global oneCounter = 0;  
+
 
 ## output_precision(16);
 
@@ -20,7 +28,7 @@ global fRec0 = [0,0];
 ##
 function pole = tau2pole (tau)
   global SR;
-  pole = exp(-1 / (tau * SR));
+  pole = exp(-1.0 / (tau * SR));
 endfunction
 
 
@@ -42,13 +50,17 @@ endfunction
 ##
 ##
 function envASRFE = asrfe (attT60,susLvl,relT60,finLvl,gate)
+  global zeroCounter;
+  global oneCounter;  
   ugate = gt(gate,0);
   if (ugate == 0)
+    zeroCounter++;
     target = finLvl;
-    t60 = attT60;
+    t60 = relT60;
   elseif (ugate == 1)
     target = susLvl;
-    t60 = relT60;
+    t60 = attT60;
+    oneCounter++;
   endif
   pole = tau2pole(t60/6.91);
   envASRFE = smooth(pole, target);
@@ -75,7 +87,7 @@ a_ = 0.005;
 r_ = (len - (a_*SR))/SR; #0.1718;
 s_ = 0.1;
 f_ = 0.05;
-gateSignal1 = ones(1, a_*SR);
+gateSignal1 = ones(1, round(a_*SR));
 gateSignal0 = zeros(1, round(r_*SR));
 gateSignal = horzcat(gateSignal1,gateSignal0);
 
@@ -85,10 +97,12 @@ for i = 1:length(gateSignal)
   envARE(i) = are(a_,r_,gateSignal(i));  
 endfor
 
-
-plot([1:length(gateSignal)],envARE);
+tvec = [1:length(gateSignal)]';
+plot(tvec,[gateSignal',envARE', faustout]);
 ylim([-1.0,1.0]);
-xlim([0,4000]);
+xlim([0,len]);
 grid on; grid minor on;
 
 
+printf("zeroCounter = %d\n",zeroCounter);
+printf("oneCounter = %d\n",oneCounter);
